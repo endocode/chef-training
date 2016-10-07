e843a4caecf2e3cad0f5c0c9f0231a71dd35153d
Sebastian Sucker <sebastian@endocode.com>
2016-10-07 17:19:52 +0200

    Adds windows vms Vagrantfile & config.

---
+++ tools/students_vms_windows/cookbooks/windows-example/metadata.rb
@@ -0,0 +1,10 @@
+name 'windows-example'
+maintainer 'Sebastian Sucker'
+maintainer_email 'sebastian@endocode.com'
+license 'MIT'
+description 'example windows cookbook'
+long_description 'shows basic chef usage for windows plattform'
+version '0.0.1'
+
+depends 'windows'
+depends 'chocolatey'
---
+++ tools/students_vms_windows/cookbooks/windows-example/recipes/default.rb
@@ -0,0 +1,4 @@
+include_recipe 'windows'
+include_recipe 'chocolatey'
+
+chocolatey_package '7zip'
